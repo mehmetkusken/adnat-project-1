36a16e0456d2097e6fdca118e90fa29e4376f622
Mehmet <mehmetkusken@gmail.com>
2022-07-04 19:29:04 -0600

    created shift controller and view

--- app/controllers/shifts_controller.rb
+++ app/controllers/shifts_controller.rb
@@ -1,2 +1,26 @@
 class ShiftsController < ApplicationController
+
+    def index
+        @shifts = Shift.all
+    end
+   
+    def new
+        @shift = Shift.new
+    end
+
+    def create
+        @shift = Shift.create(shift_params)
+        if @shift.save
+        redirect_to "http://[::1]:3000/shifts"
+        else
+            render :new
+        end
+    end
+    
+    private
+
+    def shift_params
+        params.require(:shift).permit(:start, :finish, :break_length)
+    end
+
 end
