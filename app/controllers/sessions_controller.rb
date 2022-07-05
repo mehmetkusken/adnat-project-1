55d3194a9aae013a1ebac3685813654cb049ec26
Mehmet <mehmetkusken@gmail.com>
2022-07-05 17:12:05 -0600

    created validation and error messages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,11 +7,12 @@ class SessionsController < ApplicationController
     def login
        
     end
+    
     def create
         user = User.find_by_email(params[:email])
         if user && user.authenticate(params[:password])
             session[:user_id] = user.id
-            redirect_to organisations
+            redirect_to organisations_path
         else
 
             redirect_to login_path
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
   belongs_to :organisation
   has_secure_password
+  validates :email, :name, uniqueness: true
+  validates :email, :name, presence: true
+  validates :password, length: { in: 6..22 }
 end
