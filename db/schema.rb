cb7f1742a0bf5ada2cc6900f2eed112c9399f73c
Mehmet <mehmetkusken@gmail.com>
2022-07-04 19:11:54 -0600

    added user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_05_011001) do
+ActiveRecord::Schema.define(version: 2022_07_05_011122) do
 
   create_table "organisations", force: :cascade do |t|
     t.string "name"
@@ -19,4 +19,15 @@ ActiveRecord::Schema.define(version: 2022_07_05_011001) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.integer "organisation_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["organisation_id"], name: "index_users_on_organisation_id"
+  end
+
+  add_foreign_key "users", "organisations"
 end
