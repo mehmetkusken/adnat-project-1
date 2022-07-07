62c65379882bee660fdfdc61cded6770b0162677
Mehmet <mehmetkusken@gmail.com>
2022-07-07 01:54:22 -0600

    made forgot password section

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,10 @@ Rails.application.routes.draw do
   delete "/logout", to: "sessions#logout", as: "logout"
   get "/login", to: "sessions#login", as: "login"
   post "/login", to: "sessions#create"
+
+  get "password/reset", to: "password_resets#new"
+  post "password/reset", to: "password_resets#create"
+  get "password/reset/edit", to: "password_resets#edit"
+  patch "password/reset/edit", to: "password_resets#update"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
